63dc1c9d0c2b557faa9ac7979d193d9d2c2faa07
ColinLPH <colin.lph.lam@gmail.com>
2025-11-07 21:09:28 -0800

    added logging, send and recv all

--- src/main.c
+++ src/main.c
@@ -6,6 +6,33 @@
 
 #define PORT 12345
 
+ssize_t send_all(int sock, const unsigned char *buf, size_t len) {
+    size_t sent = 0;
+    while (sent < len) {
+        ssize_t n = send(sock, buf + sent, len - sent, 0);
+        if (n <= 0) return -1;
+        sent += n;
+    }
+    return sent;
+}
+
+ssize_t recv_all(int sock, unsigned char *buf, size_t len) {
+    size_t recvd = 0;
+    while (recvd < len) {
+        ssize_t n = recv(sock, buf + recvd, len - recvd, 0);
+        if (n <= 0) return -1;
+        recvd += n;
+    }
+    return recvd;
+}
+
+void print_hex(const char *label, const unsigned char *buf, size_t len) {
+    printf("%s: ", label);
+    for (size_t i = 0; i < len; i++)
+        printf("%02x", buf[i]);
+    printf("\n");
+}
+
 int main(int argc, char *argv[]) {
     if (sodium_init() < 0) {
         printf("libsodium initialization failed\n");
@@ -13,7 +40,7 @@ int main(int argc, char *argv[]) {
     }
 
     if (argc < 2) {
-        printf("Usage: %s <server_ip> [port default 12345]\n", argv[0]);
+        printf("Usage: %s <server_ip> [port]\n", argv[0]);
         return 1;
     }
 
@@ -23,10 +50,7 @@ int main(int argc, char *argv[]) {
     printf("Connecting to %s:%d...\n", serverIP, server_port);
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("socket failed");
-        return 1;
-    }
+    if (sockfd < 0) { perror("socket failed"); return 1; }
 
     struct sockaddr_in servaddr = {0};
     servaddr.sin_family = AF_INET;
@@ -47,9 +71,10 @@ int main(int argc, char *argv[]) {
     unsigned char client_pk[crypto_kx_PUBLICKEYBYTES];
     unsigned char client_sk[crypto_kx_SECRETKEYBYTES];
     crypto_kx_keypair(client_pk, client_sk);
+    print_hex("Client PK", client_pk, crypto_kx_PUBLICKEYBYTES);
 
-    // Send client public key to server
-    if (send(sockfd, client_pk, crypto_kx_PUBLICKEYBYTES, 0) != crypto_kx_PUBLICKEYBYTES) {
+    // Send client public key
+    if (send_all(sockfd, client_pk, crypto_kx_PUBLICKEYBYTES) != crypto_kx_PUBLICKEYBYTES) {
         perror("send failed");
         close(sockfd);
         return 1;
@@ -57,11 +82,12 @@ int main(int argc, char *argv[]) {
 
     // Receive server public key
     unsigned char server_pk[crypto_kx_PUBLICKEYBYTES];
-    if (recv(sockfd, server_pk, crypto_kx_PUBLICKEYBYTES, 0) != crypto_kx_PUBLICKEYBYTES) {
+    if (recv_all(sockfd, server_pk, crypto_kx_PUBLICKEYBYTES) != crypto_kx_PUBLICKEYBYTES) {
         perror("recv failed");
         close(sockfd);
         return 1;
     }
+    print_hex("Server PK", server_pk, crypto_kx_PUBLICKEYBYTES);
 
     // Derive shared secret
     unsigned char rx[crypto_kx_SESSIONKEYBYTES], tx[crypto_kx_SESSIONKEYBYTES];
@@ -71,10 +97,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("Shared secret (rx) for encryption:\n");
-    for (int i = 0; i < crypto_kx_SESSIONKEYBYTES; i++)
-        printf("%02x", rx[i]);
-    printf("\n");
+    print_hex("Client RX (receive key)", rx, crypto_kx_SESSIONKEYBYTES);
+    print_hex("Client TX (transmit key)", tx, crypto_kx_SESSIONKEYBYTES);
 
     close(sockfd);
     return 0;
